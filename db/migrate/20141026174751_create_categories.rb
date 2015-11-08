ccba6ffcb6f0c101b2efaae27e448bf28fbba525
lacostenycoder <djlancejordan@gmail.com>
2015-11-08 14:43:53 -0500

    get rid of migration files as schema will set up original database structure as was ported from old mysql .sql files

--- db/migrate/20141026174751_create_categories.rb
+++
@@ -1,9 +0,0 @@
-class CreateCategories < ActiveRecord::Migration
-  def change
-    create_table :categories do |t|
-      t.string :name
-      t.integer :row_order
-      t.timestamps
-    end
-  end
-end
