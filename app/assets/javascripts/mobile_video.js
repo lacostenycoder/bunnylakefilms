1655e92ff268bae51dc4cd017d798d24e4578707
lacostenycoder <djlancejordan@gmail.com>
2016-01-14 15:53:59 -0500

    change mobile video size. add hover effect on play button

--- app/assets/javascripts/mobile_video.js
+++ app/assets/javascripts/mobile_video.js
@@ -8,23 +8,26 @@ function setEventListners(){
     player.click(function(){
       var replaceMe = $(this);
       var code = replaceMe.data('videoCode');
-      var video = mobilePlayer(code);
+      var title = replaceMe.data('videoTitle');
+      var video = mobilePlayer(code, title);
       replaceMe.html(video);
     });
   });
 }
 
-function playerDesktop(videoId){
+function playerDesktop(videoId, title){
   var str = '<iframe id="video-player" src="//player.vimeo.com/video/';
     str += videoId;
     str += '"';
     str += 'width="320" height="auto" frameborder="0" byline="0" color="ce3229" title="0" badge="0" autoplay="true" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""> </iframe>';
+    str += ("<br/>" + title);
     return str;
 }
 
-function mobilePlayer(videoId){
+function mobilePlayer(videoId, title){
   var str = '<iframe id="video-player" src="//player.vimeo.com/video/';
     str += videoId;
-    str += '" width="100%" height="auto" frameborder="0" byline="0" color="ce3229" title="0" badge="0" autoplay="1" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>';
+    str += '" width="320px" height="320px" frameborder="0" byline="0" color="ce3229" title="0" badge="0" autoplay="1" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>';
+    str += ("<br/>" + title);
     return str;
 }
