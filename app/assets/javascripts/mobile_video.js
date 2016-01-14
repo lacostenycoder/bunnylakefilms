d933db4e26c7a6bddcc25d0fe526406421b68787
lacostenycoder <djlancejordan@gmail.com>
2016-01-14 01:25:27 -0500

    commit mobile_video.js on merge branch

---
+++ app/assets/javascripts/mobile_video.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  // var mobileDivs = getMobileDivs();
+  // mobileDivs.each(function(e){
+  //   var that = e;
+  //   console.log(that);
+  // });
+  $('.mobile-player').each(function(){
+    var player = $(this);
+    player.click(function(){
+      var replaceMe = $(this);
+      var code = replaceMe.data('videoCode');
+      var video = playerString(code);
+      replaceMe.html(video);
+    });
+  });
+});
+
+function playerString(videoId){
+  var str = '<iframe id="video-player" src="//player.vimeo.com/video/'
+    str += videoId;
+    str += '"';
+    str += 'width="320" height="auto" frameborder="0" byline="0" color="ce3229" title="0" badge="0" autoplay="true" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""> </iframe>'
+    return str;
+}
