fe9b3e9980828bbeb6cf41917d697205fcf64090
lacostenycoder <djlancejordan@gmail.com>
2016-01-15 20:13:06 -0500

    precompile path in assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-
+Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .otf)
 # HTML5 Shiv
 # Rails.application.config.assets.precompile += %w( html5shiv.js )
 
