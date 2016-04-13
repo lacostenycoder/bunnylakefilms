504c09618a090c600a3e208e000ce54e8e22ee01
lacostenycoder <djlancejordan@gmail.com>
2016-04-13 07:10:58 -0400

    use work_status other to hide works_status on main works drop down

--- app/models/work_status.rb
+++ app/models/work_status.rb
@@ -2,6 +2,8 @@ class WorkStatus < ActiveRecord::Base
   has_many :works
   accepts_nested_attributes_for :works
 
-  default_scope { order(:row_order) }
-
+  default_scope { order(:row_order).published }
+  
+  scope :published, -> { where("name != ?", 'Other') }
+  
 end
