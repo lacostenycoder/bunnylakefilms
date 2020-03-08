f9d1f1f777a8e4addefe5b193c8ab837650e317a
Lance Jordan <lance@thoroughcare.net>
2020-03-07 19:45:14 -0500

    fix fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,12 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.version = '1.1'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( .svg .eot .woff .ttf .otf)
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
 # HTML5 Shiv
 # Rails.application.config.assets.precompile += %w( html5shiv.js )
 
